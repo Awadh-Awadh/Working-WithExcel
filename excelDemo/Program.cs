8c9642f1c22ce11286eb648ba77090563ff5818a
Awadh Awadh <awadhawadh84@yahoo.com>
2022-07-18 10:56:09 +0300

    create initial data

---
+++ excelDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using excelDemo;using OfficeOpenXml;
+
+ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+var file = new FileInfo(@"C:\Users\AwadhSaid\Desktop\Demos\learn.xlsx");
+
+var people = GetSetUpData();
+
+static List<PersonModel> GetSetUpData()
+{
+    List<PersonModel> output = new()
+    {
+        new() { Id = 1, FirstName = "Awadh", LastName = "Said" },
+        new() { Id = 2, FirstName = "John", LastName = "Kamau" },
+        new() { Id = 3, FirstName = "Dan", LastName = "Nakola" },
+        new() { Id = 4, FirstName = "Elisha", LastName = "Misoi" }
+    };
+    return output;
+}
